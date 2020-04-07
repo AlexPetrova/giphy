81c0ade49c5c27d6d981ee7aefb67a3ada537f71
AlexPetrova <a.petrova95@yahoo.com>
2020-04-07 18:29:59 +0300

    add gifList component

---
+++ src/components/gifList/gifList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./gifList.css";
+import { GIFList as GifListProps } from '../../types';
+import { GIFCard } from '../gif';
+
+export function GIFList(props: GifListProps) {
+    return (
+        <div className="gif-cards">
+            {props.gifCards.map(card => {
+                return <GIFCard key={card.id}
+                    id={card.id}
+                    title={card.title}
+                    importedDaysAgo={card.importedDaysAgo}
+                    url={card.url}
+                />
+            })}
+        </div>
+    )
+}
