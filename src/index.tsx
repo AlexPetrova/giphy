b00373fe6362abfc0f00933f31e6186908caf910
AlexPetrova <a.petrova95@yahoo.com>
2020-04-28 16:46:52 +0300

    update index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { GIFCard } from './types';
+import { getGifProps } from './data/transformations';
+import { Section } from './components/section';
+
+let gifCards: GIFCard[] = [
+  {
+    id: "iJyDdK1q9zdnLtzOBO",
+    title: "Jill Scott Shimmy GIF by BET",
+    url: "https://media3.giphy.com/media/YrenQlURSKKbZXjtyB/200.webp?cid=11c8cb106b5ec6d33822cfd1dba00c806e896429ab751b63&rid=200.webp",
+    importedDaysAgo: "2 days ago",
+    isLiked: false
+  },
+  {
+    id: "2ishoJbg7ljjAwZ1Q6",
+    title: "Adorable GIF by MOODMAN",
+    url: "https://media2.giphy.com/media/1fY6aevf8CBIK5XMs1/200.gif?cid=11c8cb106b5ec6d33822cfd1dba00c806e896429ab751b63&rid=200.gif",
+    importedDaysAgo: "3 days ago",
+    isLiked: false
+  },
+  {
+    id: "wzxK9cmYgIPDy",
+    title: "Brady Bunch K GIF",
+    url: "https://media1.giphy.com/media/SXUlcDoE1Bl6xsjNr6/200.gif?cid=11c8cb106b5ec6d33822cfd1dba00c806e896429ab751b63&rid=200.gif",
+    importedDaysAgo: "6 days ago",
+    isLiked: false
+  },
+  {
+    id: "ZYETZYvIk3L2CoSxIM",
+    title: "John Mulaney Snl GIF by Saturday Night Live",
+    url: "https://media2.giphy.com/media/322FvxfciE8UsYvILG/200.gif?cid=11c8cb106b5ec6d33822cfd1dba00c806e896429ab751b63&rid=200.gif",
+    importedDaysAgo: "9 days ago",
+    isLiked: false
+  }
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Section title="Trending" />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
