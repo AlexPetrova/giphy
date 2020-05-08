20dbb20e930d389d5ae890c25f193720b4c7498a
AlexPetrova <a.petrova95@yahoo.com>
2020-05-08 19:33:28 +0300

    use props instead state for like status

--- src/components/like/like.tsx
+++ src/components/like/like.tsx
@@ -4,28 +4,26 @@ import "./like.css";
 
 export function Like(props: LikeProps) {
 
-    const [isLiked, setLiked] = useState(props.isLiked);
-    const classNames = `${isLiked ? "liked-text fade-in" : "like-text"}`;
+    const classNames = `${props.isLiked ? "liked-text fade-in" : "like-text"}`;
 
     return (
         <div className="like-container">
             <img
-                src={isLiked
+                src={props.isLiked
                     ? "/heart-filled.svg"
                     : "/heart.svg"}
                 alt="heart icon"
                 className="like-heart"
                 onClick={() => {
-                    setLiked(!isLiked);
                     props.onLikeClick(prevState => {
-                        return isLiked
+                        return props.isLiked
                             ? prevState.filter(value => value !== props.id)
                             : [...prevState, props.id]
                     });
                 }}
             />
             <span className={classNames}>
-                {isLiked ? "Liked!" : "Like"}
+                {props.isLiked ? "Liked!" : "Like"}
             </span>
         </div>
     )
